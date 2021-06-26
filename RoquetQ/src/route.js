31e7b374372b29562ff61d211876c9c01b36d3b0
Christian Todesquine Dias <todesquinechristian@gmail.com>
2021-06-26 16:16:41 -0300

    Fix: update all routes to ejs files

--- RoquetQ/src/route.js
+++ RoquetQ/src/route.js
@@ -3,6 +3,8 @@ const express = require('express')
 const route = express.Router();
 
 route.get('/', (req, res) => res.render("index"))
+route.get('/create-pass', (req, res) => res.render("create-pass"))
+route.get('/room', (req, res) => res.render("room"))
 
 
 module.exports = route
