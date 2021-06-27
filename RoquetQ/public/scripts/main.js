ad67ad6206e0df5c9d38dd7b6c8f1e169a8ee4c5
Christian Todesquine Dias <todesquinechristian@gmail.com>
2021-06-27 11:49:38 -0300

    Fix: update the events of the modal

--- RoquetQ/public/scripts/main.js
+++ RoquetQ/public/scripts/main.js
@@ -2,19 +2,34 @@ import Modal from './modal.js'
 
 const modal = Modal()
 
+const modalTitle = document.querySelector('.modal h2')
+const modalDescription = document.querySelector('.modal p')
+const modalButton = document.querySelector('.modal button')
+
+
 const checkButtons = document.querySelectorAll(".actions a.check")
 
 checkButtons.forEach(button => {
-    button.addEventListener("click", event => { 
-        modal.open()
-    })
+    button.addEventListener("click", handleClick)
 })
 
 
 const deleteButtons = document.querySelectorAll(".actions a.delete")
 
 deleteButtons.forEach(button => {
-    button.addEventListener("click", event => { 
-        modal.open()
-    })
+    button.addEventListener("click", (event) => handleClick(event, false))
 })
+
+function handleClick(event, check = true) {
+    event.preventDefault()
+
+    const text = check ? "Marcar como lida" : "Excluir"
+
+    modalTitle.innerHTML = `${text}`
+    modalDescription.innerHTML = check ? `Tem certeza que deseja ${text.toLowerCase()} esta pergunta?` : `Tem certeza que deseja ${text.toLowerCase()} esta pergunta?`
+    modalButton.innerHTML = `Sim, ${text.toLowerCase()}`
+
+    check ? modalButton.classList.remove("red") : modalButton.classList.add("red")
+
+    modal.open()
+}
