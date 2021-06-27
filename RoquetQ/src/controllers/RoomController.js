8a71ad5bf29f7bec49d3c200543e1002e7f0c309
Christian Todesquine Dias <todesquinechristian@gmail.com>
2021-06-27 15:10:47 -0300

    Fix: update RoomController.js with exist condition

--- RoquetQ/src/controllers/RoomController.js
+++ RoquetQ/src/controllers/RoomController.js
@@ -5,19 +5,30 @@ module.exports = {
         const db = await Database()
         const pass = req.body.password
         let roomId
+        let isRoom = true
 
-        for(var i = 0; i < 6; i++){
-            i == 0 ? roomId = Math.floor(Math.random() * 10).toString() : 
-            roomId += Math.floor(Math.random() * 10).toString()
+        while(isRoom) {
+            
+            for(var i = 0; i < 6; i++){
+                i == 0 ? roomId = Math.floor(Math.random() * 10).toString() : 
+                roomId += Math.floor(Math.random() * 10).toString()
+            }
+    
+            const roomsExistIds = await db.all(`SELECT id FROM rooms`)
+    
+            isRoom = roomsExistIds.some(roomExistId => roomExistId === roomId)
+    
+            if(!isRoom){
+                await db.run(`INSERT INTO rooms (
+                    id,
+                    pass
+                )VAlUES (
+                    ${parseInt(roomId)},
+                    ${pass}
+                )`)
+            }
         }
 
-        await db.run(`INSERT INTO rooms (
-            id,
-            pass
-        )VAlUES (
-            ${parseInt(roomId)},
-            ${pass}
-        )`)
 
         await db.close()
 
