8ac998f753b898b4fb83f78e708abead51baa103
Christian Todesquine Dias <todesquinechristian@gmail.com>
2021-06-27 12:44:48 -0300

    Feat: create controllers and Question Controller

--- RoquetQ/src/route.js
+++ RoquetQ/src/route.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const QuestionController = require('./controllers/QuestionController')
 
 const route = express.Router();
 
@@ -7,7 +8,7 @@ route.get('/create-pass', (req, res) => res.render("create-pass"))
 route.get('/room', (req, res) => res.render("room"))
 
 // Formato que o formulario de dentro da modal tem q passar a informacao
-// route.post('/room/:room/:question/:action')
+route.post('/room/:room/:question/:action', QuestionController.index)
 
 
 module.exports = route
